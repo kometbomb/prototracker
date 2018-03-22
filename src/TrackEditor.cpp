eb5058f544307833b3e2e6881ca81f589e7b56d6
Tero Lindeman <kometbomb@kometbomb.net>
2018-03-22 21:45:17 +0200

    Fixed checking of gamepad support in SDL

--- src/TrackEditor.cpp
+++ src/TrackEditor.cpp
@@ -372,7 +372,7 @@ bool TrackEditor::onEvent(SDL_Event& event)
 				break;
 			}
 
-#ifdef SDL_GameControllerFromInstanceID
+#if SDL_VERSION_ATLEAST(2,0,4)
 			case SDL_CONTROLLERBUTTONDOWN:
 			{
 				bool aPressed = SDL_GameControllerGetButton(SDL_GameControllerFromInstanceID(event.cbutton.which), SDL_CONTROLLER_BUTTON_A);
