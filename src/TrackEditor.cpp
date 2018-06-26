2b8c02da3fbe78a42365db25f5468f1a73cdeb31
Tero Lindeman <kometbomb@kometbomb.net>
2018-06-26 18:31:24 +0300

    Fixed weird coloring

--- src/TrackEditor.cpp
+++ src/TrackEditor.cpp
@@ -610,7 +610,7 @@ void TrackEditor::onDraw(Renderer& renderer, const SDL_Rect& area)
 		int columnX = renderer.getFontWidth() * mTrackEditorState.currentColumn;
 
 		SDL_Rect textArea = {mTrackEditorState.currentTrack * trackWidth + area.x + rowNumberWidth + columnX, area.y + centerY, columnWidth, rowHeight};
-		renderer.renderRect(textArea, mEditorState.editMode ? Theme::ColorType::EditCursor : Theme::ColorType::NonEditCursor);
+		renderer.clearRect(textArea, mEditorState.editMode ? Theme::ColorType::EditCursor : Theme::ColorType::NonEditCursor);
 	}
 
 	for (int track = 0 ; track < maxTracks ; ++track)
@@ -630,10 +630,10 @@ void TrackEditor::onDraw(Renderer& renderer, const SDL_Rect& area)
 			else if (isRowActive(track, row))
 			{
 				// Highlight current play row green
-				renderer.renderRect(textArea, Theme::ColorType::PlayHead);
+				renderer.clearRect(textArea, Theme::ColorType::PlayHead);
 			}
 
-			renderPatternRow(renderer, textArea, patternRow, color);
+			renderPatternRow(renderer, textArea, patternRow, renderer.getTheme().getColor(color));
 		}
 	}
 }
